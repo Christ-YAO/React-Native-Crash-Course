88a54c4f532e9c5b22aa9d7d96991a77ca3e123d
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-12-23 15:04:34 +0000

    Tailwind configuration with babel

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,13 @@
 import { Link } from "expo-router";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 
 export default function App() {
 
     return (
-        <View style={styles.container}>
+        <View className="flex-1 items-center justify-center bg-white">
             <StatusBar barStyle="dark-content" />
             <Text>Aora!</Text>
             <Link href="/profile" style={{ color: "blue", textDecorationLine: "underline" }}>Go to Profile</Link>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
